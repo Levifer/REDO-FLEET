fe80a8c03d3e8c58319ed96e1c3249a684c82a18
Levifer <b.pieteraerents@gmail.com>
2014-10-29 15:55:52 +0100

    Added the car and custompack entity

--- src/main/java/com/realdolmen/domain/Car.java
+++ src/main/java/com/realdolmen/domain/Car.java
@@ -1,7 +1,50 @@
 package com.realdolmen.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by TLAAU71 on 29/10/2014.
  */
+@Entity
 public class Car {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    private Model model;
+    private Integer mileage;
+    @Temporal(TemporalType.DATE)
+    private Date carInUse;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+
+    public Date getCarInUse() {
+        return carInUse;
+    }
+
+    public void setCarInUse(Date carInUse) {
+        this.carInUse = carInUse;
+    }
 }
--- src/main/java/com/realdolmen/domain/CustomPack.java
+++ src/main/java/com/realdolmen/domain/CustomPack.java
@@ -1,7 +1,32 @@
 package com.realdolmen.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
 /**
  * Created by BPTAT47 on 29/10/2014.
  */
+@Entity
 public class CustomPack {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @OneToMany
+    private List<Option> options;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<Option> options) {
+        this.options = options;
+    }
 }
