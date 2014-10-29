b153dc161192f79465696f652b6899f2cdc27bb3
Levifer <b.pieteraerents@gmail.com>
2014-10-29 15:44:44 +0100

    Added employee and enums

--- src/main/java/com/realdolmen/domain/Enums.java
+++ src/main/java/com/realdolmen/domain/Enums.java
@@ -4,4 +4,35 @@ package com.realdolmen.domain;
  * Created by BPTAT47 on 29/10/2014.
  */
 public class Enums {
+
+    public enum Roles {
+        EMPLOYEE_APPROVED("employee.name"),
+        FLEET_MANAGER("manager.name");
+
+        private final String label;
+
+        Roles(String label) {
+            this.label = label;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
+
+    public enum Status {
+        APPROVED("employee.Approved"),
+        DISAPPROVED("employee.Disapproved");
+
+        private final String label;
+
+        Status(String label) {
+            this.label = label;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
+
 }
