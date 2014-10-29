c9c1341ca24984189729ad0ec38179c112b76a10
Tim Lemahieu <tim.lemahieu@Realdolmen.com>
2014-10-29 15:29:11 +0100

    Entities

---
+++ src/main/java/com/realdolmen/domain/Model.java
@@ -0,0 +1,143 @@
+package com.realdolmen.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by TLAAU71 on 29/10/2014.
+ */
+@Entity
+public class Model {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String brand;
+    private String type;
+    private Fuel fuelType;
+    private Date year;
+    private int quarter,
+    private Pack pack;
+    private int minKm;
+    private int maxKm;
+    private int deliveryTime;
+    private int fiscalHp;
+    private int co2;
+    private int category;
+
+    public Model() {
+    }
+
+    public Model(String brand, String type, Fuel fuelType, Date year, int quarter, Pack pack, int minKm, int maxKm, int deliveryTime, int fiscalHp, int co2, int category) {
+        this.brand = brand;
+        this.type = type;
+        this.fuelType = fuelType;
+        this.year = year;
+        this.quarter = quarter;
+        this.pack = pack;
+        this.minKm = minKm;
+        this.maxKm = maxKm;
+        this.deliveryTime = deliveryTime;
+        this.fiscalHp = fiscalHp;
+        this.co2 = co2;
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Fuel getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(Fuel fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public int getQuarter() {
+        return quarter;
+    }
+
+    public void setQuarter(int quarter) {
+        this.quarter = quarter;
+    }
+
+    public Pack getPack() {
+        return pack;
+    }
+
+    public void setPack(Pack pack) {
+        this.pack = pack;
+    }
+
+    public int getMinKm() {
+        return minKm;
+    }
+
+    public void setMinKm(int minKm) {
+        this.minKm = minKm;
+    }
+
+    public int getMaxKm() {
+        return maxKm;
+    }
+
+    public void setMaxKm(int maxKm) {
+        this.maxKm = maxKm;
+    }
+
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public int getFiscalHp() {
+        return fiscalHp;
+    }
+
+    public void setFiscalHp(int fiscalHp) {
+        this.fiscalHp = fiscalHp;
+    }
+
+    public int getCo2() {
+        return co2;
+    }
+
+    public void setCo2(int co2) {
+        this.co2 = co2;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+}
---
+++ src/main/java/com/realdolmen/domain/Option.java
@@ -0,0 +1,62 @@
+package com.realdolmen.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * Created by TLAAU71 on 29/10/2014.
+ */
+@Entity
+public class Option {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Type type;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public Option() {
+    }
+
+    public Option(Type type, String name, String description, BigDecimal price) {
+        this.type = type;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
