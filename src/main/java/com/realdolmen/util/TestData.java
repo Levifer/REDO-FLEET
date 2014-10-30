45a576048c36c1eec292db6b909a5ea148d82e81
Levifer <b.pieteraerents@gmail.com>
2014-10-30 16:06:40 +0100

    Additional testdata for carmodel, needs to be more specific

--- src/main/java/com/realdolmen/util/TestData.java
+++ src/main/java/com/realdolmen/util/TestData.java
@@ -1,7 +1,10 @@
 package com.realdolmen.util;
 
 import com.realdolmen.domain.Enums;
+import com.realdolmen.domain.car.Car;
+import com.realdolmen.domain.carmodel.CarModel;
 import com.realdolmen.domain.option.Option;
+import com.realdolmen.domain.option.OptionService;
 import com.realdolmen.domain.pack.Pack;
 import com.realdolmen.domain.pack.PackService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,9 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -21,6 +27,8 @@ import java.util.Random;
 public class TestData implements ApplicationListener<ContextRefreshedEvent> {
 
 
+    @Autowired
+    private OptionService optionService;
     @Autowired
     private PackService packService;
     @PersistenceContext
@@ -32,8 +40,8 @@ public class TestData implements ApplicationListener<ContextRefreshedEvent> {
 
     }
 
-    public Random randomGenerator() {
-        return new Random();
+    public BigDecimal randomGenerator() {
+        return new BigDecimal(new Random().nextInt(500));
     }
 
     public void insertOptions() {
@@ -54,19 +62,21 @@ public class TestData implements ApplicationListener<ContextRefreshedEvent> {
                 "Steering wheels are used in most modern land vehicles, including all mass-production automobiles, as well as busses, light and heavy trucks, and tractors. The steering wheel is the part of the steering system that is manipulated by the driver; the rest of the steering system responds to such driver inputs. This can be through direct mechanical contact as in recirculating ball or rack and pinion steering gears, without or with the assistance of hydraulic power steering, HPS, or as in some modern production cars with the assistance of computer controlled motors, known as Electric Power Steering.";
         final String description5 = "A tire pressure monitoring system (TPMS) is an electronic system designed to monitor the air pressure inside the pneumatic tires on various types of vehicles. TPMS report real-time tire-pressure information to the driver of the vehicle, either via a gauge, a pictogram display, or a simple low-pressure warning light. TPMS can be divided into two different types — direct (dTPMS) and indirect (iTPMS). TPMS are provided both at an OEM (factory) level as well as an aftermarket solution. ";
 
-        Option option = new Option(Enums.Technical.BRAKES.getLabel(), "ABS", description, new BigDecimal(randomGenerator().nextInt(500)));
-        Option option2 = new Option(Enums.Technical.SUPPORT_SYSTEMS.getLabel(), "ESP", description2, new BigDecimal(randomGenerator().nextInt(500)));
-        Option option3 = new Option(Enums.Interior.OTHER.getLabel(), "7 Airbag", description3, new BigDecimal(randomGenerator().nextInt(500)));
-        Option option4 = new Option(Enums.Interior.STEERING_WHEEL.getLabel(), "Leather", description4, new BigDecimal(randomGenerator().nextInt(500)));
-        Option option5 = new Option(Enums.Exterior.WHEELS.getLabel(), "Pressure monitoring", description5, new BigDecimal(randomGenerator().nextInt(500)));
+        Option option = new Option(Enums.Technical.BRAKES.getLabel(), "ABS", description, randomGenerator());
+        Option option2 = new Option(Enums.Technical.SUPPORT_SYSTEMS.getLabel(), "ESP", description2, randomGenerator());
+        Option option3 = new Option(Enums.Interior.OTHER.getLabel(), "7 Airbag", description3, randomGenerator());
+        Option option4 = new Option(Enums.Interior.STEERING_WHEEL.getLabel(), "Leather", description4, randomGenerator());
+        Option option5 = new Option(Enums.Exterior.WHEELS.getLabel(), "Pressure monitoring", description5, randomGenerator());
         entityManager.persist(option);
         entityManager.persist(option2);
         entityManager.persist(option3);
         entityManager.persist(option4);
         entityManager.persist(option5);
-           
-        Pack pack = new Pack()
+        List<Option> optionsFromTheDB =  optionService.findAll();
+        Pack pack = new Pack(packService.getPrice(optionsFromTheDB),randomGenerator(),randomGenerator(),randomGenerator(),optionsFromTheDB);
+        entityManager.persist(pack);
 
+        CarModel carModel = new CarModel("Audi","A3", Enums.Fuel.DIESEL,new Date(),1,140000,180000,20,9,88,2,"",pack,new ArrayList<Car>());
 
     }
 
