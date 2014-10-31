fdaf58d41752d19d52fec04b81f96f069c846fd9
Aveline Estié <aveline.estie@realdolmen.com>
2014-10-31 10:36:39 +0100

    login to order

--- src/main/java/com/realdolmen/controller/LoginController.java
+++ src/main/java/com/realdolmen/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.realdolmen.controller;
 
+import com.realdolmen.util.LoggerProducer;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    @LoggerProducer
+    private Logger logger;
+
     @RequestMapping("/login")
     public String getLoginPage(Model model){
+        logger.info("getLoginPage");
         model.addAttribute("isLoggedIn",false);
         return "login";
     }
